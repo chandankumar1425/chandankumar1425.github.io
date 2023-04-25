b91f7f6b1857aa83a70fef29544063bc40113bd1
Chandan Kumar Behera <118505620+chandankumar1425@users.noreply.github.com>
2023-04-25 16:45:58 +0530

    nav

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ $(document).ready(function(){
         var redirectw1 = window.open('https://drive.google.com/file/d/1SE2qqDdIdkg07ec1m7Oloi_jYZ06-Y5q/view?usp=share_link');
         redirectw1.location;
     }
+
+    ////////////////////////
+    var typed = new Typed(".multi-text", {
+        strings: ["Full Stack Web Developer", "Full Stack Web Developer", "Node Backend Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+
